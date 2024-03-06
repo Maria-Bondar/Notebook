a2040ff79d84cd3f8763057541650112615a5557
Maria <python.sites111@gmail.com>
2024-03-06 13:27:59 +0200

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\vasyl\OneDrive\Desktop\Марічка\Python\Applications\Notebook2\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
